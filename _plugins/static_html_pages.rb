9d1c57f8d2b4373399d3d1c806a0731bca60dc34
jonathanfrei <hi@jonathanfrei.com>
2026-08-21 14:46:20 -0400

    one one more time trying

--- _plugins/static_html_pages.rb
+++ _plugins/static_html_pages.rb
@@ -66,7 +66,7 @@ module Jekyll
     safe true
     priority :low
 
-    DEFAULT_ROOTS = %w[_pages/editorial].freeze
+    DEFAULT_ROOTS = %w[_pages].freeze
 
     def generate(site)
       roots = roots_for(site)
